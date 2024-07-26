a49f44ebc356c96fccf1e8aabbf7a65fad515864
Sergey <salvan26@mail.ru>
2024-07-26 22:42:11 +0300

    swiperApp

---
+++ swiper.js
@@ -0,0 +1,56 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const resizableSwiper = (breakpoint, swiperClass, swiperSettings) => {
+      let swiper;
+  
+      breakpoint = window.matchMedia(breakpoint);
+  
+      const enableSwiper = function(className, settings) {
+        swiper = new Swiper(className, settings);
+      }
+  
+      const checker = function() {
+        if (breakpoint.matches) {
+          return enableSwiper(swiperClass, swiperSettings);
+        } else {
+          if (swiper !== undefined) swiper.destroy(true, true);
+          return;
+        }
+      };
+  
+  
+      breakpoint.addEventListener('change', checker);
+      checker();
+    }
+  
+    resizableSwiper(
+      '(max-width: 760px)',
+      '.swiper-container',
+      {
+      direction: 'horizontal',
+      loop: true,
+      loopedSlides: 11,
+      slidesPerView: 1.5,
+      spaceBetween: 15,
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+      },
+      }, 
+    );
+  });
+  
+  
+  const showMore = document.querySelector('.show-more');
+  showMore.addEventListener('click', function () { 
+    let content = document.querySelector('.swiper-wrapper');
+    if (content.classList.contains ('wrapper-show') )  {
+        content.classList.remove('wrapper-show')
+        showMore.textContent = 'Показать все';
+    } else {
+        content.classList.add('wrapper-show');
+        showMore.textContent = 'Скрыть';  
+    }
+  });
